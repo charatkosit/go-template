6bad55b665d840c616aa5256fa0b736b73d33a76
charat <charatkosit@gmail.com>
2022-10-08 10:18:14 +0700

    check point 2

--- src/app/modules/shopping/components/shopping-cart/shopping-cart.component.ts
+++ src/app/modules/shopping/components/shopping-cart/shopping-cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/interfaces/product';
+import { Cart } from '../../../../interfaces/cart';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -8,19 +9,35 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./shopping-cart.component.css']
 })
 export class ShoppingCartComponent implements OnInit {
-  total:number =0;
+  total: number = 0;
   cartList: Product[] = [];
+  cart: Cart[] = []   // ยิงแบบตัวเดียว กับยิงขอเป็นชุด datatype ไม่เหมือนกัน
+  tcart: any
+  arr: string[] = [];
+  // myCart_dummy:number[] =[12,4,5,7,14];
 
+  myCart_Product: number[] = [12, 4, 5, 7, 14];
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    let url = "https://fakestoreapi.com/products?limit=5";
-    this.http.get<Product[]>(url).subscribe(res => {
-    
-      this.cartList = res;
-      this.total =this.cartList.reduce((accumulator,obj)=>{ return accumulator +obj.price},0)
+    console.log("Hello")
+    this.myCart_Product.forEach(id => {
+      let url = "https://fakestoreapi.com/products/" + id;
+      console.log("url: " + url)
+
+      this.http.get<Cart[]>(url).subscribe(res => {
+        console.log(res);
+        this.tcart = res;
+        this.cart.push(this.tcart);
+        this.total = this.cart.reduce((accumulator, obj) => {
+          return accumulator + obj.price
+        }, 0)
+      });
+      console.log(this.cart);
 
     })
   }
+
+
 
 }
