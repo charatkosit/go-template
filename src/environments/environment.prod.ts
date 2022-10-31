8b6e557c865f962fc5e14480b15579d1e8267900
charat <charatkosit@gmail.com>
2022-10-31 21:38:59 +0700

    test1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
   sapApiToken:"z@hz3sNY#0ohB9SspeE9@fLDQ%r65x$k8LxL28VH72FfvRWgCn",
   loginResult: "loginResult",
   baseUrl: "",
-  project: "Build Prod:v0.1.3 on 27/10/22",
+  project: "Build Prod:v0.1.3a on 27/10/22",
 };
