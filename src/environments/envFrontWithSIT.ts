8cf1e5f609b92d13212a11c7cc8c995f3f8df0a7
charat <charatkosit@gmail.com>
2022-11-17 21:41:48 +0700

    release0.0.26

--- src/environments/envFrontWithSIT.ts
+++ src/environments/envFrontWithSIT.ts
@@ -1,19 +1,20 @@
 #!/bin/bash
 #prepare  data
-release=`cat /var/lib/jenkins/workspace/go-front-sit/package.json |grep "version" |cut -d'"' -f 4`
+tagname=`cat /var/lib/jenkins/workspace/go-front-sit/package.json |grep "version" |cut -d'"' -f 4`
 
-backendUrl='http://localhost:8085/'
+backendUrl='http://goapisit.ddns.net:3000/'
 sapUrl='http://192.168.20.17:8880/apigoplus/EnqPartlist/'
 sapApiToken='z@hz3sNY#0ohB9SspeE9@fLDQ%r65x$k8LxL28VH72FfvRWgCn'
 stage='SIT' 
 timestamp=$(date)
-release=$release
 
 #replace data
-sed -i -e "s#%release%#$release#g" environment.prod.ts
-sed -i -e "s|%backendUrl%|${backendUrl}|g" environment.prod.ts
-sed -i -e "s|%sapUrl%|${sapUrl}|g" environment.prod.ts
-sed -i -e "s|%sapApiToken%|${sapApiToken}|g" environment.prod.ts
-sed -i -e "s|%stage%|${stage}|g" environment.prod.ts
-sed -i -e "s|%timestamp%|${timestamp}|g" environment.prod.ts
-sed -i -e "s|%release%|${release}|g" environment.prod.ts
+#sed -i -e "s#%release%#$release#g" environment.prod.ts
+sed -i -e "s|%backendUrl%|${backendUrl}|g" /var/lib/jenkins/workspace/go-front-sit/src/environments/environment.prod.ts
+sed -i -e "s|%sapUrl%|${sapUrl}|g" /var/lib/jenkins/workspace/go-front-sit/src/environments/environment.prod.ts
+sed -i -e "s|%sapApiToken%|${sapApiToken}|g" /var/lib/jenkins/workspace/go-front-sit/src/environments/environment.prod.ts
+sed -i -e "s|%stage%|${stage}|g" /var/lib/jenkins/workspace/go-front-sit/src/environments/environment.prod.ts
+sed -i -e "s|%timestamp%|${timestamp}|g" /var/lib/jenkins/workspace/go-front-sit/src/environments/environment.prod.ts
+sed -i -e "s|%tagname%|${tagname}|g" /var/lib/jenkinks/workspace/go-front-sit/src/environments/environment.prod.ts
+#debug
+cat /var/lib/jenkins/workspace/go-front-sit/src/environments/environment.prod.ts
