1c77e44f0eab89eb3b485223ea5775eb35aa4979
charat <charatkosit@gmail.com>
2022-11-17 19:14:19 +0700

    release0.0.23

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  backendUrl: "http://localhost:8085/",
-  sapUrl: "http://192.168.20.17:8880/apigoplus/EnqPartlist/",
-  sapApiToken:"z@hz3sNY#0ohB9SspeE9@fLDQ%r65x$k8LxL28VH72FfvRWgCn",
+  backendUrl: "%backendUrl%",
+  sapUrl: "%sapUrl%",
+  sapApiToken:"%sapApiToken%",
   loginResult: "loginResult",
   baseUrl: "",
-  project: "Build Prod:v0.0.22 on 17/11/22",
-  release: "0.0.22",
+  project: "Build %stage%:%timestamp%",
+  release: "%release%",
 };
