91682cf41090a70fb1d23c35b09c6d8f12cc35e2
charat <charatkosit@gmail.com>
2022-11-18 16:21:58 +0700

    release 0.0.30

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  release:string = "";
   constructor() { }
+ 
 
   ngOnInit(): void {
+     this.release=environment.release;
   }
 
 }
