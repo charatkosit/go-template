c2629ae040820add2c8385b7bd2ae7191dbcb408
charat <charatkosit@gmail.com>
2022-11-22 10:38:36 +0700

    release0.0.41

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,10 +11,12 @@ import { environment } from 'src/environments/environment';
 export class HeaderComponent implements OnInit {
 
    project:string ='';
+   backUrl:string = '';
   constructor(private router:Router, public rest:RestService) { }
 
   ngOnInit(): void {
      this.project = environment.project;
+     this.backUrl = environment.backendUrl;
   }
 
  
